2c7720b51d880d7a7d0ec5665c0f31ca6943acac
pr1vi3t <quemaditopls17@gmail.com>
2025-01-07 22:40:06 -0500

    dasadsasd

--- src/app.js
+++ src/app.js
@@ -9,8 +9,16 @@ const io = socket(httpServer);
 app.use(express.static('public'));
 
 io.on('connection', async (socket)=>{
-    const {id, handshake} = socket;
-    console.log(`Se ha conectado [${id}] [${handshake.address}] - [${handshake.time}]`)
+    const { id, handshake } = socket;
+    console.log(`Se ha conectado [${id}] [${handshake.address}] - ${handshake.time}`);
+
+    socket.on('evt_usuarioLogin',async (usuario)=>{
+        console.log('Se ha logueado el usuario', usuario);
+        io.emit('evt_usuarioLogin',{
+            ...usuario, 
+            time: handshake.time
+        });
+    });
 });
 
-module.exports = httpServer;    
+module.exports = httpServer; 
