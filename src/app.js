fa907240c34d602be46945173738bb7e9c3b9049
pr1vi3t <quemaditopls17@gmail.com>
2025-01-08 21:12:00 -0500

    h

--- src/app.js
+++ src/app.js
@@ -12,11 +12,23 @@ io.on('connection', async (socket)=>{
     const { id, handshake } = socket;
     console.log(`Se ha conectado [${id}] [${handshake.address}] - ${handshake.time}`);
 
+    //RECIBO EVENTO LOGIN ENVIADO DESDE EL USUARIO X
     socket.on('evt_usuarioLogin',async (usuario)=>{
         console.log('Se ha logueado el usuario', usuario);
+        //AVISO A LOS DEMAS USUARIOS QUE EL USUARIO X SE HA CONECTADO
         io.emit('evt_usuarioLogin',{
             ...usuario, 
-            time: handshake.time
+            fechaHora: handshake.time
+        });
+    });
+
+    //RECIBO EVENTO MENSAJE DESDE EL USUARIO X
+    socket.on('evt_usuarioMensaje', async(mensaje)=>{
+        console.log('Se ha recibido el mensaje', mensaje);
+        io.emit('evt_usuarioMensaje', {
+            ...mensaje,
+            fechaHora: new Date(),
+            idSocketRemitente: id
         });
     });
 });
